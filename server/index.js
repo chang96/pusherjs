56a49c3a2132187b715ab5cd9902b461ca94aaab
chang <rorewole@gmail.com>
2020-12-31 15:23:49 +0100

    minmum necessary code done

---
+++ server/index.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const Pusher = require("pusher")
+const PushNotifications = require('@pusher/push-notifications-server');
+const generateVotes = require("./votegenerator");
+const mySampleRegions = [
+  {regionname: '1', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+  {regionname: '2', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+  {regionname: '3', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+  {regionname: '4', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+  {regionname: '5', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+  {regionname: '6', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+  {regionname: '7', population:2189499, turnoutpercent: 51, totalvotes: 1116644, currentvotes: 0},
+]
+const sampleData = [
+  [ 1, '1J5FVCE', 52, 4, 19, 10, 52,4, 19, 10, 'inprogress',0 ],
+  [ 1, '2J5FVCE', 15, 6, 10, 12, 67,10,29,22 , 'inprogress', 1 ],
+  [ 1, '3J5FVCE', 25, 6, 3, 21, 92,16,32,43, 'inprogress' ,2],
+  [ 1, '4J5FVCE', 35, 14, 8, 12,127,30,40,55 , 'inprogress',3 ],
+  [ 1, '5J5FVCE', 4, 16, 8, 21,131,46,48,71 , 'inprogress',4 ],
+  [ 1, '6J5FVCE', 12, 36, 8, 22, 143, 82,56,93, 'completed',5 ]
+]
+const allRegions = [
+  '1J5FVCE',
+'2J5FVCE',
+'3J5FVCE',
+'4J5FVCE',
+'5J5FVCE',
+'6J5FVCE',
+]
+const app = express()
+const PORT = process.env.PORT || 3000
+const pusher = new Pusher({
+    appId: "1124234",
+    key: "b691171de5f8ac605664",
+    secret: "9b22dfa2b49d99cabeb2",
+    cluster: "mt1",
+    useTLS: true
+  });
+(async function(){
+  const votes = await generateVotes()
+  const spliced = votes.splice(0, 35)
+  console.log(spliced[0], votes[4])
+})()
+var i = 0
+
+setInterval(() => {
+  if(i == 5){
+    i = 0
+  }
+  pusher.trigger("my-channel", "my-event", {
+    message: sampleData[i]
+}).then(console.log).catch(e=> console.log(e))
+  i++
+}, 50000);
+
+let beamsClient = new PushNotifications({
+  instanceId: '6e70a6a0-a057-4c9d-b88d-95963f7fe209',
+  secretKey: 'A31A51C3FD12980D9F72517FA2FBD9294A7CDC66F7076C6E3FB852F9C00B49C0'
+});
+
+// beamsClient.publishToInterests(['hello'], {
+//   web: {
+//     notification: {
+//       title: 'Hello',
+//       body: 'Hello, world!',
+//       deep_link: 'https://www.pusher.com',
+//     }
+//   }
+// }).then((publishResponse) => {
+//   console.log('Just published:', publishResponse.publishId);
+// }).catch((error) => {
+//   console.log('Error:', error);
+// });
+app.listen(PORT, ()=> console.log("running"))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import States from "./userComponents/states/index"
+import * as PusherPushNotifications from "@pusher/push-notifications-web"
+import Pusher from 'pusher-js';
+import Nav from "./userComponents/header/nav/nav";
+import Footer from "./userComponents/footer/footer";
 
 function App() {
+  const sampleData = [
+    [ 1, '1J5FVCE', 0, 0, 0, 0, 0, 0, 0, 0, 'inprogress' ],
+    [ 1, '2J5FVCE', 0, 0, 0, 0, 0, 0, 0, 0, 'inprogress' ],
+    [ 1, '3J5FVCE', 0, 0, 0, 0, 0, 0, 0, 0, 'inprogress' ],
+    [ 1, '4J5FVCE', 0, 0, 0, 0, 0, 0, 0, 0, 'inprogress' ],
+    [ 1, '5J5FVCE', 0, 0, 0, 0, 0, 0, 0, 0, 'inprogress' ],
+    [ 1, '6J5FVCE', 0, 0, 0, 0, 0, 0, 0, 0, 'inprogress' ]
+  ]
+  const [data, updateData] = useState({message: sampleData})
+  const [total, updateTotal] = useState({message: sampleData[0]})
+
+  const pusher = new Pusher('b691171de5f8ac605664', {
+    cluster: 'mt1'
+  });
+  const channel = pusher.subscribe('my-channel');
+  channel.bind('my-event', function(dataFromServer) {
+    updateData(data =>{
+      console.log(dataFromServer+"fghjkjhgf", data)
+      return {message: data.message.splice(Number(dataFromServer.message[11]), 1, dataFromServer.message)}
+    }
+    )
+    updateTotal(dataFromServer)
+    console.log(dataFromServer)
+    // alert(dataFromServer.message)
+    
+  });
+  const beamsClient = new PusherPushNotifications.Client({
+    instanceId: '6e70a6a0-a057-4c9d-b88d-95963f7fe209',
+  })
+  beamsClient.start()
+  .then(() => beamsClient.getDeviceId())
+  .then(deviceId => {
+    console.log(deviceId) // Will log something like web-1234-1234-1234-1234
+  }).catch(e => console.error('Could not get device id', e));
+  
+  beamsClient.start()
+    .then(() => beamsClient.addDeviceInterest('hello'))
+    .then(() => {
+      // Build something beatiful 🌈
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div><Nav /></div>
+      <div style={{display:"flex", justifyContent:"space-around"}}><States total={total} data={data} /></div>
+      <div><Footer/></div>
     </div>
   );
 }
